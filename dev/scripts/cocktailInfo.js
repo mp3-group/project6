d1f7a31746a104b4ec1ba6c971ddd89d0f266e53
Amber Lahav <amberlahav@gmail.com>
2017-11-28 16:46:37 -0500

    made a few component files

---
+++ dev/scripts/cocktailInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CocktailInfo extends React.Component {
+    render(){
+        return(
+            <h2>hi</h2>
+        )
+    }
+}
+
+export default CocktailInfo;
