1826cbd9dc04eda5f6879d258b5b7d55cf241e92
kermy <kermy19800@gmail.com>
2017-12-04 12:37:50 -0500

    commiting changes

--- dev/scripts/gallery.js
+++ dev/scripts/gallery.js
@@ -35,6 +35,7 @@ class Gallery extends React.Component {
                 },
 
                 q: `coffee ${alcohol}`
+                
             }
         }).then((res) => {
             this.setState({
@@ -181,7 +182,6 @@ class CocktailInfo extends React.Component {
             </div>
         )
     }
-
 }
 
 export default Gallery;
